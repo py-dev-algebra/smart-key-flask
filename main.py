f4ebe7543503a26b9a7576e7d85f22dc10206625
py-dev-algebra <python.developer.edukacija@gmail.com>
2024-03-26 18:52:02 +0000

    Init

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+# http://www.domena.hr/
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# http://www.domena.hr/about
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
